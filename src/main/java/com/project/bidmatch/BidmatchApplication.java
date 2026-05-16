4488afc352fbcc9734ea78ebe9fb3367acf07e31
Seungyoung <leesy960909@gmail.com>
2026-05-16 10:32:32 +0900

    first commit

---
+++ src/main/java/com/project/bidmatch/BidmatchApplication.java
@@ -0,0 +1,13 @@
+package com.project.bidmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidmatchApplication.class, args);
+	}
+
+}
